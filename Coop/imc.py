88d78612d3e887f03fa8c745570ac02a369deb36
droppxr <bielbi67@gmail.com>
2025-12-21 18:32:40 -0300

    Adicionei a logica

--- Coop/imc.py
+++ Coop/imc.py
@@ -6,6 +6,21 @@ Dev A: Cria imc.py. Faz a parte que pede o Peso e a Altura do usuário (float). 
 Dev B: Dá git pull. Adiciona o cálculo: imc = peso / (altura ** 2). Imprime o valor do IMC na tela. Comita e Push.
 
 Dev A: Dá git pull. Adiciona as condições (if imc < 18.5: print("Abaixo do peso"), etc.). Comita e Push. '''
+
+# Print inicial
 print('Para calcular seu IMC:')
+
+# Inputs
 peso = float(input('Digite seu peso (kg): '))
 altura = float(input('Digite sua altura (cm): '))
+
+# Logica
+    # Converter altura para metros
+
+altura_em_metros = 0
+
+    # IMC
+imc = peso / altura_em_metros ** 2
+
+
+print(f'Seu IMC é {imc}')
