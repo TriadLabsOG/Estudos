e187ccd3b54fbff3b3b0eb2716b10f46c24b9ba2
LuscasPY <lucaspierrevieira@gmail.com>
2025-12-21 18:29:57 -0300

    adicionei um print novo no ex 43

--- Lucas/CursoEmVideo/ex043.py
+++ Lucas/CursoEmVideo/ex043.py
@@ -1,6 +1,7 @@
 peso = float(input('Qual é o seu peso? '))
 altura = float(input('Qual é a sua altura? '))
 imc = peso / altura**2
+print(f'seu imc é de {imc}')
 if imc <18.5:
     print('Você esta abaixo do peso, alimente-se!')
 elif imc >=18.5 and imc <=25:
