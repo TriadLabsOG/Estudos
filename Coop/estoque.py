98a245defae422b993b6ed3ba242b2588281fd4e
LuscasPY <lucaspierrevieira@gmail.com>
2025-12-25 20:00:16 -0300

    fix: Arruma o dicionario, tranforma em lista

--- Coop/estoque.py
+++ Coop/estoque.py
@@ -4,19 +4,63 @@
 
 # Lucas: Aceite o PR. Na main (após Pull), crie a branch feat-busca. Adicione uma função que recebe o nome de um produto e diz se ele está no estoque. Commit, Push e PR.
 
-produtos_varejo = {
-    'Coca-Cola 600ml': 5.00,
-    'Arroz 5kg': 30.00,
-    'Feijão 1kg': 8.50,
-    'Açucar 5kg': 19.25,
-    'Azeite 500ml': 52.90,
-    'Picanha 1kg': 89.99,
-    'Ventilador': 200.00,
-    'Jogo de Taças Cristal': 55.00,
-    'Coca-Cola 2L': 11.90
-}
+produtos_varejo = [
+    {
+        'id': '1',
+        'produto': 'Coca-Cola 600ml',
+        'valor': 5.00,
+        'estoque': 81
+    },
+    {
+        'id': '2',
+        'produto': 'Arroz 5kg',
+        'valor': 30.00,
+        'estoque': 23
+    },
+    {
+        'id': '3',
+        'produto': 'Feijão 1kg',
+        'valor': 8.50,
+        'estoque': 76
+    },
+    {
+        'id': '4',
+        'produto': 'Açucar 5kg',
+        'valor': 19.25,
+        'estoque': 20
+    },
+    {
+        'id': '5',
+        'produto': 'Azeite 500ml',
+        'valor': 52.90,
+        'estoque': 10
+    },
+    {
+        'id': '6',
+        'produto': 'Picanha 1kg',
+        'valor': 89.99,
+        'estoque': 90
+    },
+    {
+        'id': '7',
+        'produto': 'Ventilador',
+        'valor': 200.00,
+        'estoque': 0
+    },
+    {
+        'id': '8',
+        'produto': 'Jogo de Taças Cristal',
+        'valor': 55.00,
+        'estoque': 20
+    },
+    {
+        'id': '9',
+        'produto': 'Coca-Cola 2L',
+        'valor': 11.90,
+        'estoque': 10
+    }
+]
 
-print('PRODUTOS MAIS CAROS QUE 50 REAIS')
 for produto in produtos_varejo:
     if produtos_varejo[produto] > 50:
         print(f'{produto}: {produtos_varejo[produto]}')
@@ -31,3 +75,4 @@ def buscador_de_produtos(lista_produtos, produto_procurado):
 
     
 print(buscador_de_produtos(produtos_varejo, 'Picanha'))
+
