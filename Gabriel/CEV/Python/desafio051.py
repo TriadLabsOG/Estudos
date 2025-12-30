2a8d23ad5b2c3c8c5ded6785e41c7f499ff7740e
droppxr <bielbi67@gmail.com>
2025-12-30 06:22:49 -0300

    fix: mantem exercicio antigo

--- Gabriel/CEV/Python/desafio051.py
+++ Gabriel/CEV/Python/desafio051.py
@@ -1,7 +1,7 @@
 
 # ANTIGO
 
-'''print('='*40)
+print('='*40)
 print('10 TERMOS DE 1 P.A')
 print('='*40)
 
@@ -10,4 +10,4 @@ r = int(input('Digite a razão: '))
 
 for i in range(n1, 10 * r + 1, r):
     print(f'{i} > ', end='')
-print('FIM')'''
+print('FIM')
