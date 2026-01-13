3f1056d289ad0d7f1645f32a50c8dfc6fc2ff2c5
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-13 18:17:57 -0300

    fix: arruma alguns exercicios do Curso Em Vídeo

--- Lucas/CursoEmVideo/ex046.py
+++ Lucas/CursoEmVideo/ex046.py
@@ -1,4 +1,5 @@
 import time
-for c in range(10, 0, -1):
+for c in range(10, -1, -1):
     print(c)          # Primeiro mostra o número
     time.sleep(1)     # Depois espera 1 segundo
+print('BUMMMMMMMMMMMMM!')
--- Lucas/CursoEmVideo/ex047.py
+++ Lucas/CursoEmVideo/ex047.py
@@ -1,4 +1,3 @@
-import time
 lista = []
 for c in range(2, 51, 2):
     lista.append(c)
