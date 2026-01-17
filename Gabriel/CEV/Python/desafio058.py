23cffaf83ef69891172c92e39eadfdc5ea5fd19a
droppxr <bielbi67@gmail.com>
2026-01-17 12:12:04 -0300

    fix: refatora desafio058

--- Gabriel/CEV/Python/desafio058.py
+++ Gabriel/CEV/Python/desafio058.py
@@ -1,5 +1,17 @@
 # Exercício Python 058: Melhore o jogo do DESAFIO 028 onde o computador vai "pensar" em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
 
+from random import randint
+
+computador = randint(0, 10)
+contador_de_tentativas = 0
+humano = None
+
+while computador != humano:
+    contador_de_tentativas += 1
+    humano = int(input("Qual número o computador pensou? "))
+
+print(f"O número do computador era {computador} e você acertou com {contador_de_tentativas} tentativas.")
+
 """
 ANTIGO
 
