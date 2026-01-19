163c1fffce643c517842e6c32630fd4a594b0e64
droppxr <bielbi67@gmail.com>
2026-01-19 16:36:14 +0000

    feat: adiciona enunciado desafio061

--- Gabriel/CEV/Python/desafio061.py
+++ Gabriel/CEV/Python/desafio061.py
@@ -1,3 +1,12 @@
+# ==========================================================
+# DESAFIO 061: Progressão Aritmética v2.0
+#
+# Enunciado: Refaça o DESAFIO 051, lendo o primeiro termo 
+# e a razão de uma PA, mostrando os 10 primeiros termos 
+# da progressão usando a estrutura while.
+# ==========================================================
+
+
 """
 ANTIGO
 
