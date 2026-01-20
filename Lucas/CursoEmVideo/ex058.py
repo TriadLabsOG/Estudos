7a1f2cff1f677e53e308299485eded7d5ff36157
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-20 10:37:26 -0300

    feat: Adiciona o enunciado do exercicio 58

--- Lucas/CursoEmVideo/ex058.py
+++ Lucas/CursoEmVideo/ex058.py
@@ -1,3 +1,5 @@
+# Exercício Python 58: Melhore o jogo do DESAFIO 28 onde o computador vai “pensar” em um número entre 0 e 10. Só que agora o jogador vai tentar adivinhar até acertar, mostrando no final quantos palpites foram necessários para vencer.
+
 from random import randint
 from time import sleep
 computador = randint(0, 10) # O computador pensa em um numero de 0 a 10
