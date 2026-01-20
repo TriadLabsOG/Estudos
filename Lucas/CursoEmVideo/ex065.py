06aee6926dbf82e2b2db253d3191a9687c66df1f
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-20 12:26:08 -0300

    fix: Corrige alguns erros no codigo

--- Lucas/CursoEmVideo/ex065.py
+++ Lucas/CursoEmVideo/ex065.py
@@ -1,18 +1,31 @@
-numero = int(input('Digite um valor: '))
+# Exercício Python 65: Crie um programa que leia vários números inteiros pelo teclado. No final da execução, mostre a média entre todos os valores e qual foi o maior e o menor valores lidos. O programa deve perguntar ao usuário se ele quer ou não continuar a digitar valores.
+
+
+lista = []
 
 media_total = 0
 
 acumuladora = 0
 
-resposta = str(input('Você deseja continuar a digitar valores?[S/N]')).capitalize()
+resposta = 'S'
 
 while resposta == 'S':
     numero = int(input('Digite um valor: '))
-
+    
+    lista.append(numero)
+    
     resposta = str(input('Você deseja continuar a digitar valores?[S/N] ')).capitalize()
 
     media_total += 1
 
     acumuladora += numero
 
-print(f'A media de todos os numeros digitados foi de {acumuladora / media_total}')
+    
+if len(lista) > 0:
+    media = sum(lista) / len(lista)
+    maior = max(lista)
+    menor = min(lista)
+
+print(f'Você digitou {len(lista)} números, com a media sendo de {media}.', end= '')
+print(f'E o maior número foi o {maior} e o menor número foi o {menor}')
+
