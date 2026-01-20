1e8b412b29f2e067fa7f5e49963cdc25eed29861
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-20 11:35:52 -0300

    feat: Adiciona o enunciado do exercicio 62

--- Lucas/CursoEmVideo/ex062.py
+++ Lucas/CursoEmVideo/ex062.py
@@ -1,3 +1,5 @@
+# Exercício Python 62: Melhore o DESAFIO 61, perguntando para o usuário se ele quer mostrar mais alguns termos. O programa encerrará quando ele disser que quer mostrar 0 termos.
+
 termo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
 
