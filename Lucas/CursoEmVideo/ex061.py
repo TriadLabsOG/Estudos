2ae234da0bea216d24efba572ea4dcc7db9543d5
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-20 11:34:23 -0300

    feat: Adiciona o enunciado do exercicio 61

--- Lucas/CursoEmVideo/ex061.py
+++ Lucas/CursoEmVideo/ex061.py
@@ -1,7 +1,9 @@
+# Exercício Python 61: Refaça o DESAFIO 51, lendo o primeiro termo e a razão de uma PA, mostrando os 10 primeiros termos da progressão usando a estrutura while.
+
 termo = int(input('Digite o primeiro termo: '))
 razao = int(input('Digite a razão: '))
 
-contador = 0
+contador = 1
 
 while contador <= 10:
     print(f'{termo}', end= ' -> ' if contador < 10 else ' -> FIM!')
