fab3dc5e88a71c12bc6a108aa67256320f01b264
droppxr <bielbi67@gmail.com>
2026-01-21 21:00:46 -0300

    fix: refatora desafio064

--- Gabriel/CEV/Python/desafio064.py
+++ Gabriel/CEV/Python/desafio064.py
@@ -1,4 +1,34 @@
 """
+DESAFIO 064: Tratando Vários Valores v1.0
+
+Crie um programa que leia vários números inteiros pelo teclado. 
+O programa só vai parar quando o usuário digitar o valor 999, 
+que é a condição de parada. 
+
+No final, mostre quantos números foram digitados e qual foi a soma 
+entre eles (desconsiderando o flag 999).
+"""
+from time import sleep
+
+lista = []
+numero = 0
+print("DIGITE '999' PARA PARAR!")
+
+while numero != 999:
+    numero = int(input('Digite um número: '))
+    lista.append(numero)
+
+print('Encerrando', end='', flush=True)
+sleep(0.5)
+
+for pontos in range(0, 3):
+    print('.', end='', flush=True)
+    sleep(0.5)
+
+print(f'\n\nQUANTIDADE DE NÚMEROS DIGITADOS: {len(lista)-1}')
+print(f'SOMA: {sum(lista)-999}')
+
+"""
 ANTIGO
 
 numero = 0
@@ -13,5 +43,4 @@ while numero_chave != 999:
 quantidade_numeros -= 1
 numero -= 999
  
-print(f'Você digitou {quantidade_numeros} números e a soma deles é {numero}')
-"""
+print(f'Você digitou {quantidade_numeros} números e a soma deles é {numero}')"""
