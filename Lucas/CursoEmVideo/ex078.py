3581d868861db8e05237334f7c9140cc49ba5357
LuscasPY <lucaspierrevieira@gmail.com>
2026-01-24 18:24:43 -0300

    fix: Arruma o exercicio 78, adicionando em qual posição o menor e o maior aparecem

--- Lucas/CursoEmVideo/ex078.py
+++ Lucas/CursoEmVideo/ex078.py
@@ -1,14 +1,14 @@
 valores = []
 
-contador = 0
 
-while contador != 5: # Eu fiz de uma maneira mais complexa para eu praticar mais o while
-    valor = float(input('Digite um valor: '))
+for v in range(0, 5):
+    valor = int(input('Digite um valor: '))
+
     valores.append(valor)
-    contador += 1
 
-menor = min(valores)
+print(f'O maior número foi o {max(valores)} na posição {valores.index(max(valores))}')
+print(f'O menor número foi o {min(valores)} na posição {valores.index(min(valores))}')
 
-maior = max(valores)
+    
 
-print(f'O maior valor digitado foi o {maior} e o menor foi o {menor}!')
+
