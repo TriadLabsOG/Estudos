284c0cfb0062246f061c14dab4ea17a381746b27
LuscasPY <lucaspierrevieira@gmail.com>
2026-02-01 10:00:05 -0300

    feat: Adiciona o exercicio 080

---
+++ Lucas/CursoEmVideo/ex080.py
@@ -0,0 +1,20 @@
+numeros = []
+
+for c in range(0, 5):
+    numero = int(input('Digite um valor: '))
+    
+    if c == 0 or numero > numeros[-1]:
+        numeros.append(numero)
+        print('Adicionado ao final da lista...')
+        
+    else:
+        posicao = 0
+        while posicao < len(numeros):
+            if numero <= numeros[posicao]:
+                numeros.insert(posicao, numero)
+                print(f'Adicionado na posição {posicao} da lista...')
+                break
+            posicao += 1
+
+print('-=' * 30)
+print(f'Os valores digitados em ordem foram: {numeros}')
