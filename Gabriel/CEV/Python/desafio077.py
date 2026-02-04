d1ad39449cdf991b62d2f375e1b570e230960ebd
droppxr <bielbi67@gmail.com>
2026-02-04 20:52:55 +0000

    fix: comenta o desafio077.py

--- Gabriel/CEV/Python/desafio077.py
+++ Gabriel/CEV/Python/desafio077.py
@@ -1,5 +1,6 @@
 # Exercício Python 77: Exercício Python 077: Crie um programa que tenha uma tupla com várias palavras (não usar acentos). Depois disso, você deve mostrar, para cada palavra, quais são as suas vogais.
 
+# TUPLA COM OS ITENS
 tupla = (
         "ovo",
         "salada",
@@ -11,9 +12,14 @@ tupla = (
         "abobora"
         )
 
+# LOOP PALAVRA POR PALAVRA E ADICIONANDO NO PRINT
 for palavra in tupla:
     print(f'VOGAIS DE {palavra.upper()}: ', end='')
+
+    # LOOP LETRA POR LETRA CONFERINDO SE TEM VOGAIS COM UM IF
     for i in palavra:
         if i in 'aeiou':
             print(f'{i} ', end='')
+
+    # ATALHO PARA QUEBRAR LINHA
     print('')
