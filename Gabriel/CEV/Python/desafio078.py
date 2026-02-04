af094dc97be9af7a0f8ee87314df6a93dd404f31
droppxr <bielbi67@gmail.com>
2026-02-04 21:09:55 +0000

    feat: adiciona logica para ler 5 valores no desafio078.py

--- Gabriel/CEV/Python/desafio078.py
+++ Gabriel/CEV/Python/desafio078.py
@@ -1,1 +1,8 @@
 # Exercício Python 78: Exercício Python 078: Faça um programa que leia 5 valores numéricos e guarde-os em uma lista. No final, mostre qual foi o maior e o menor valor digitado e as suas respectivas posições na lista.
+
+# Logica para ler os 5 valores
+lista = []
+while len(lista) < 5:
+    lista.append(input('Digite um valor: '))
+
+# Logica para ordenar do maior até o menor
