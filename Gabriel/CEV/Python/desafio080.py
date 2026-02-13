2f3d1e7d85c8c0844339b3c1657b33bb46c1de3c
droppxr <bielbi67@gmail.com>
2026-02-12 22:21:31 -0300

    feat: adiciona loops de organização

--- Gabriel/CEV/Python/desafio080.py
+++ Gabriel/CEV/Python/desafio080.py
@@ -5,5 +5,12 @@ lista_numeros = []
 
 # Adiciona 5 números a lista
 while len(lista_numeros) != 5:
-    lista_numeros.append(input(f"Digite o {len(lista_numeros)+1}º número: "))
+    lista_numeros.append(int(input(f"Digite o {len(lista_numeros)+1}º número: ")))
+
+# Itera sobre os números (0, 4)
+for index in range(len(lista_numeros) - 1):
+    for i in range(len(lista_numeros) - 1):
+        if lista_numeros[i] > lista_numeros[i + 1]:
+            lista_numeros[i], lista_numeros[i + 1] = lista_numeros[i+1], lista_numeros[i]
+
 print(lista_numeros)
